a217f1964fd70031b3c70d97945f98197736f6b4
Jochen Mader <jochen.mader@codecentric.de>
2016-07-08 11:44:34 +0200

    Added unit tests for HandlerOps

--- src/test/scala/io/vertx/lang/scala/json/TestVerticle.scala
+++ src/test/scala/io/vertx/lang/scala/json/TestVerticle.scala
@@ -9,7 +9,7 @@ import io.vertx.lang.scala.ScalaVerticle
 class TestVerticle extends ScalaVerticle{
   override def start(startFuture: Future[Void]): Unit = {
     println("DOOONE")
-    vertx.createHttpServer.requestHandler(req => req.response.end("hello world")).listen(8877, println)
+    vertx.createHttpServer.requestHandler(req => req.response.end("hello world")).listen(8877, x => println(x))
     startFuture.complete()
   }
 }
