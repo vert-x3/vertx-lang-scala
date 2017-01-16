de352b5e4bd7688f8d9bb9abe3784860ffc8afe9
Jochen Mader <pflanzenmoerder@gmail.com>
2017-01-16 21:55:23 +0100

    Fixed broken build after some changes were merged to vertx-web. Also added vertx-web-client and all template engines

--- vertx-lang-scala-stack/vertx-service-discovery/src/main/resources/vertx-service-discovery-js/http_endpoint.js
+++ vertx-lang-scala-stack/vertx-service-discovery/src/main/resources/vertx-service-discovery-js/http_endpoint.js
@@ -17,6 +17,7 @@
 /** @module vertx-service-discovery-js/http_endpoint */
 var utils = require('vertx-js/util/utils');
 var ServiceDiscovery = require('vertx-service-discovery-js/service_discovery');
+var WebClient = require('vertx-web-client-js/web_client');
 var HttpClient = require('vertx-js/http_client');
 
 var io = Packages.io;
@@ -131,6 +132,60 @@ HttpEndpoint.getClient = function() {
   }, utils.convParamJsonObject(__args[2]), function(ar) {
     if (ar.succeeded()) {
       __args[3](utils.convReturnVertxGen(HttpClient, ar.result()), null);
+    } else {
+      __args[3](null, ar.cause());
+    }
+  });
+  } else throw new TypeError('function invoked with invalid arguments');
+};
+
+/**
+ Convenient method that looks for a HTTP endpoint and provides the configured . The async result
+ is marked as failed is there are no matching services, or if the lookup fails. This method accepts a
+ configuration for the HTTP client.
+
+ @memberof module:vertx-service-discovery-js/http_endpoint
+ @param discovery {ServiceDiscovery} The service discovery instance 
+ @param filter {todo} The filter 
+ @param conf {Object} the configuration of the client 
+ @param resultHandler {function} The result handler 
+ */
+HttpEndpoint.getWebClient = function() {
+  var __args = arguments;
+  if (__args.length === 3 && typeof __args[0] === 'object' && __args[0]._jdel && (typeof __args[1] === 'object' && __args[1] != null) && typeof __args[2] === 'function') {
+    JHttpEndpoint["getWebClient(io.vertx.servicediscovery.ServiceDiscovery,io.vertx.core.json.JsonObject,io.vertx.core.Handler)"](__args[0]._jdel, utils.convParamJsonObject(__args[1]), function(ar) {
+    if (ar.succeeded()) {
+      __args[2](utils.convReturnVertxGen(WebClient, ar.result()), null);
+    } else {
+      __args[2](null, ar.cause());
+    }
+  });
+  }else if (__args.length === 3 && typeof __args[0] === 'object' && __args[0]._jdel && typeof __args[1] === 'function' && typeof __args[2] === 'function') {
+    JHttpEndpoint["getWebClient(io.vertx.servicediscovery.ServiceDiscovery,java.util.function.Function,io.vertx.core.Handler)"](__args[0]._jdel, function(jVal) {
+    var jRet = __args[1](utils.convReturnDataObject(jVal));
+    return jRet;
+  }, function(ar) {
+    if (ar.succeeded()) {
+      __args[2](utils.convReturnVertxGen(WebClient, ar.result()), null);
+    } else {
+      __args[2](null, ar.cause());
+    }
+  });
+  }else if (__args.length === 4 && typeof __args[0] === 'object' && __args[0]._jdel && (typeof __args[1] === 'object' && __args[1] != null) && (typeof __args[2] === 'object' && __args[2] != null) && typeof __args[3] === 'function') {
+    JHttpEndpoint["getWebClient(io.vertx.servicediscovery.ServiceDiscovery,io.vertx.core.json.JsonObject,io.vertx.core.json.JsonObject,io.vertx.core.Handler)"](__args[0]._jdel, utils.convParamJsonObject(__args[1]), utils.convParamJsonObject(__args[2]), function(ar) {
+    if (ar.succeeded()) {
+      __args[3](utils.convReturnVertxGen(WebClient, ar.result()), null);
+    } else {
+      __args[3](null, ar.cause());
+    }
+  });
+  }else if (__args.length === 4 && typeof __args[0] === 'object' && __args[0]._jdel && typeof __args[1] === 'function' && (typeof __args[2] === 'object' && __args[2] != null) && typeof __args[3] === 'function') {
+    JHttpEndpoint["getWebClient(io.vertx.servicediscovery.ServiceDiscovery,java.util.function.Function,io.vertx.core.json.JsonObject,io.vertx.core.Handler)"](__args[0]._jdel, function(jVal) {
+    var jRet = __args[1](utils.convReturnDataObject(jVal));
+    return jRet;
+  }, utils.convParamJsonObject(__args[2]), function(ar) {
+    if (ar.succeeded()) {
+      __args[3](utils.convReturnVertxGen(WebClient, ar.result()), null);
     } else {
       __args[3](null, ar.cause());
     }
