d8e7b715bbfd4c28c9cc176cb6bb382b6fffbacc
Jonathan Willis <willisjtc@gmail.com>
2017-02-16 18:08:48 +0100

    Reduced test boilerplate

--- vertx-lang-scala/src/test/scala/io/vertx/lang/scala/verticle/ScalaVerticleTest.scala
+++ vertx-lang-scala/src/test/scala/io/vertx/lang/scala/verticle/ScalaVerticleTest.scala
@@ -5,22 +5,24 @@ import java.util.concurrent.{CountDownLatch, TimeUnit}
 import io.vertx.scala.core.Vertx
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{AsyncFlatSpec, FlatSpec, Matchers}
 import io.vertx.lang.scala.ScalaVerticle._
 import io.vertx.lang.scala.VertxExecutionContext
 
+import scala.util.{Failure, Success}
+
 @RunWith(classOf[JUnitRunner])
-class ScalaVerticleTest extends FlatSpec with Matchers {
+class ScalaVerticleTest extends AsyncFlatSpec with Matchers {
 
   "TestVerticle" should "reply to a hello message" in {
-    val cl = new CountDownLatch(1)
     val vertx = Vertx.vertx
     implicit val exec = VertxExecutionContext(vertx.getOrCreateContext())
-    vertx.deployVerticleFuture(nameForVerticle[TestVerticle]).onComplete(t => cl.countDown())
-    cl.await()
-    val cl2 = new CountDownLatch(1)
-    vertx.eventBus.sendFuture[String]("hello", "msg").onComplete(t => cl2.countDown())
-    assert(cl2.await(100, TimeUnit.MILLISECONDS), "No answer within 100 ms")
+    vertx.deployVerticleFuture(nameForVerticle[TestVerticle])
+      .transformWith{
+        case Failure(t) => fail(t)
+        case Success(_) => vertx.eventBus.sendFuture[String]("hello", "msg")
+      }
+      .map(res => res.body() should equal("world"))
   }
 
   "nameForVerticle" should "generate the correct name to deploy a ScalaVerticle" in {
