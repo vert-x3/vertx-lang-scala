20302beda639ea2a9641b6da9da00756d4f852a1
Julien Viet <julien@julienviet.com>
2020-01-22 17:57:39 +0100

    Try to deploy vertx-lang-scala snapshots with 4 threads to work around exceeded time limit

--- .travis.deploy.artifacts.sh
+++ .travis.deploy.artifacts.sh
@@ -1,5 +1,5 @@
 PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:evaluate -Dexpression=project.version -B | grep -v '\[')
 if [[ "$PROJECT_VERSION" =~ .*SNAPSHOT ]] && [[ "${TRAVIS_BRANCH}" =~ ^master$|^[0-9]+\.[0-9]+$ ]] && [[ "${TRAVIS_PULL_REQUEST}" = "false" ]];
 then
-  mvn deploy -q -s .travis.maven.settings.xml -DskipTests -B;
+  mvn deploy -q -s -T 4 .travis.maven.settings.xml -DskipTests -B;
 fi
