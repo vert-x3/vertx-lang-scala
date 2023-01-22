67c75f0531ccdbc7e6403689e640c398600dcd97
Oliver Marienfeld <oliver.marienfeld@brandad-systems.de>
2023-01-22 19:41:26 +0100

    Use better conversion from Array to IndexedSeq

--- vertx-lang-scala/src/main/generated/scala/io/vertx/scala/core/package.scala
+++ vertx-lang-scala/src/main/generated/scala/io/vertx/scala/core/package.scala
@@ -40,7 +40,7 @@ package object core{
     def apply(fields: (String, Any)*): JsonObject = {
       val o = new JsonObject()
       fields.foreach {
-        case (key, l: Array[_]) => o.put(key, listToJsArr(l))
+        case (key, l: Array[_]) => o.put(key, listToJsArr(l.toIndexedSeq))
         case (key, l: Seq[_]) => o.put(key, listToJsArr(l))
         case (key, value) => o.put(key, value)
       }
@@ -54,7 +54,7 @@ package object core{
     def apply(fields: Any*): JsonArray = {
       val a = new JsonArray()
       fields.foreach {
-        case array: Array[_] => a.add(listToJsArr(array))
+        case array: Array[_] => a.add(listToJsArr(array.toIndexedSeq))
         case seq: Seq[_] => a.add(listToJsArr(seq))
         case f => a.add(f)
       }
