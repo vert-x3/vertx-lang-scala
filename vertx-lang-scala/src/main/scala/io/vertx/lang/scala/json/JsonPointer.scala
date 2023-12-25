fef751884cc8e1ae5e8b5ab4b419f28892e0b10c
Oliver Marienfeld <oliver_marienfeld@outlook.de>
2023-12-25 23:28:20 +0100

    Continue with JsonPointer (wip)

--- vertx-lang-scala/src/main/scala/io/vertx/lang/scala/json/JsonPointer.scala
+++ vertx-lang-scala/src/main/scala/io/vertx/lang/scala/json/JsonPointer.scala
@@ -20,7 +20,7 @@ final case class JsonPointer(private val internal: JJsonPointer):
    * @return `true` if this pointer is a parent of the given `child`. For instance,
    *         `"/properties"` pointer is parent pointer of `"/properties/parent"`
    */
-  def isParent(child: JJsonPointer): Boolean = internal.isParent(child)
+  def isParent(child: JsonPointer): Boolean = internal.isParent(child.internal)
 
   /**
    * Copy a `JsonPointer`
@@ -29,6 +29,24 @@ final case class JsonPointer(private val internal: JJsonPointer):
 
   override def toString: String = internal.toString
 
+  /**
+   * Build a <a href="https://tools.ietf.org/html/rfc6901#section-6">URI representation</a> of the JSON Pointer
+   */
+  def toURI: URI = internal.toURI
+
+  /**
+   * Return the underlying URI without the fragment
+   */
+  def getURIWithoutFragment: URI = internal.getURIWithoutFragment
+
+  /**
+   * Append an unescaped token to this pointer
+   *
+   * @param token the unescaped reference token
+   * @return a reference to this, so the API can be used fluently
+   */
+  def append(token: String): JsonPointer = JsonPointer(internal.append(token))
+
 end JsonPointer
 
 object JsonPointer:
@@ -36,32 +54,58 @@ object JsonPointer:
   /**
    * @return an empty `JsonPointer`
    */
-  def apply(): JsonPointer = create()
+  def apply(): JsonPointer = JsonPointer(io.vertx.core.json.pointer.JsonPointer.create())
 
   /**
-   * Build an empty `JsonPointer`.
+   * Builds a [[JsonPointer]] from a String.
+   * @param pointer the String representing a valid JSON pointer
+   * @return the [[JsonPointer]]
+   * @throws IllegalArgumentException if the given String is not a valid JSON pointer
    */
-  def create(): JsonPointer = JsonPointer(io.vertx.core.json.pointer.JsonPointer.create())
+  def apply(pointer: String): JsonPointer = JsonPointer(io.vertx.core.json.pointer.JsonPointer.from(pointer))
 
   /**
-   * Build a `JsonPointer` from a JSON pointer String.
+   * Builds a [[JsonPointer]] from a [[java.net.URI]].
+   * @param pointer - the URI representing a valid JSON pointer
+   * @return the [[JsonPointer]]
+   * @throws IllegalArgumentException if the given URI is not a valid JSON pointer
+   */
+  def apply(pointer: URI): JsonPointer = JsonPointer(io.vertx.core.json.pointer.JsonPointer.fromURI(pointer))
+
+  /**
+   * Same as [[apply apply()]], for compatibility with Vert.x core.
+   */
+  def create(): JsonPointer = apply()
+
+  /**
+   * Same as [[apply apply(pointer)]], for compatibility with Vert.x core.
+   * @see [[apply]]
+   */
+  def from(pointer: String): JsonPointer = apply(pointer)
+
+  /**
+   * Builds a [[JsonPointer]] from a String, returning an [[Option]]. This comes
+   * in handy in situations when it is unknown if the String contains a valid
+   * JSON pointer, e.g. if the String is a variable.
    *
    * @param pointer the String representing a pointer
    * @return [[Some Some(JsonPointer)]] if the given String was valid, else [[None]]
    */
-  def from(pointer: String): Option[JsonPointer] =
+  def fromOption(pointer: String): Option[JsonPointer] =
     try
       Some(JsonPointer(io.vertx.core.json.pointer.JsonPointer.from(pointer)))
     catch
       case iae: IllegalArgumentException => None
 
   /**
-   * Build a `JsonPointer` from a URI.
+   * Builds a [[JsonPointer]] from a URI, returning an [[Option]]. This comes
+   * in handy in situations when it is unknown if the URI contains a valid
+   * JSON pointer, e.g. if the URI is a variable.
    *
    * @param uri the uri representing the JSON pointer
    * @return [[Some Some(JsonPointer)]] if the given URI was valid, else [[None]]
    */
-  def fromURI(uri: URI): Option[JsonPointer] =
+  def fromURIOption(uri: URI): Option[JsonPointer] =
     try
       Some(JsonPointer(io.vertx.core.json.pointer.JsonPointer.fromURI(uri)))
     catch
--- vertx-lang-scala/src/test/scala/io/vertx/lang/scala/json/JsonPointerSpec.scala
+++ vertx-lang-scala/src/test/scala/io/vertx/lang/scala/json/JsonPointerSpec.scala
@@ -1,33 +1,63 @@
 package io.vertx.lang.scala.json
 
 import org.scalatest.Inside
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers
 
 import java.net.URI
 
-class JsonPointerSpec extends AnyFlatSpec, Matchers, Inside:
+class JsonPointerSpec extends AnyFunSpec, Matchers, Inside:
 
-  "JsonPointer" should "have an isRootPointer function" in :
-    JsonPointer().isRootPointer should be(true)
+  describe("JsonPointer") {
+    it("should be constructable from a String") {
+      JsonPointer("/foo/bar") shouldBe a[JsonPointer]
+      an [IllegalArgumentException] should be thrownBy JsonPointer("|foo|bar")
+    }
+    it("should be constructable from a String, returning Option for safety") {
+      JsonPointer.fromOption("/foo/bar") should matchPattern { case Some(_) => }
+      JsonPointer.fromOption("|foo|bar") should matchPattern { case None => }
+    }
+    it("should be constructable from an URI") {
+      JsonPointer(URI("/foo/bar")) shouldBe a[JsonPointer]
+      an [IllegalArgumentException] should be thrownBy JsonPointer(URI("https://vertx.io/docs/vertx-core/java/#_json_pointers"))
+    }
+    it("should be constructable from an URI, returning Option for safety") {
+      JsonPointer.fromURIOption(URI("/foo/bar")) should matchPattern { case Some(_) => }
+      JsonPointer.fromURIOption(URI("https://vertx.io/docs/vertx-core/java/#_json_pointers")) should matchPattern { case None => }
+    }
+    it("should be copy-able") {
+      JsonPointer.fromOption("/foo/bar") match
+        case Some(orig) =>
+          val copy = orig.copy()
+          copy should not be theSameInstanceAs(orig)
+          copy should equal(orig)
+        case None       => fail("should not be None")
+    }
+    it("should return the stringified pointer") {
+      JsonPointer.fromOption("/foo/bar").map(_.toString) match
+        case Some(str) => str should be("/foo/bar")
+        case None      => fail("should not be None")
+    }
+    it("should determine if it is a root pointer") {
+      JsonPointer().isRootPointer should be(true)
+      JsonPointer("/foo/bar").isRootPointer should be(false)
+    }
+    it("should determine if it is a local pointer") {
+      JsonPointer("/foo/bar").isLocalPointer should be(true)
+      JsonPointer(URI("/foo/bar")).isLocalPointer should be(false)
+    }
+    it("should determine if it is the parent of another pointer") {
+      JsonPointer("/foo").isParent(JsonPointer("/foo/bar")) should be(true)
+      JsonPointer("/foo").isParent(JsonPointer("/foo")) should be(false)
+    }
+  }
 
-  it should "be constructable from a String" in :
-    JsonPointer.from("/foo/bar") should matchPattern { case Some(_) => }
-    JsonPointer.from("|foo|bar") should matchPattern { case None => }
-
-  it should "be constructable from a URI" in :
-    JsonPointer.fromURI(URI("/foo/bar")) should matchPattern { case Some(_) => }
-    JsonPointer.fromURI(URI("https://vertx.io/docs/vertx-core/java/#_json_pointers")) should matchPattern { case None => }
-
-  it should "support the copy() function" in :
-    JsonPointer.from("/foo/bar") match
-      case Some(orig) =>
-        val copy = orig.copy()
-        copy should not be theSameInstanceAs(orig)
-        copy should equal(orig)
-      case None       => fail("should not be None")
-
-  it should """return "/foo/bar" when toString is called""" in :
-    JsonPointer.from("/foo/bar").map(_.toString) match
-      case Some(str) => str should be("/foo/bar")
-      case None      => fail("should not be None")
+  describe("Appending to a JsonPointer") {
+    val pointer = JsonPointer("/foo/bar")
+    it("should append an unescaped String") {
+      val appended = pointer.append("baz")
+      appended.toString should be("/foo/bar/baz")
+      pointer.isParent(appended) should be(true)
+      ??? // isParent nachschauen im RFC
+    }
+  }
